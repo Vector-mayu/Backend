abc889e7b92d0adf8561533580f9a0236f18e521
Vector-mayu <mayureshdandekar143@gmail.com>
2026-01-16 00:58:05 +0530

    Added DAY1 of backend

---
+++ DAY1/first.js
@@ -0,0 +1,6 @@
+import {Add, Sub} from "./second.js"
+
+Add(1,4);
+Sub(10,5);
+
+console.log("Running First")
---
+++ DAY1/second.js
@@ -0,0 +1,11 @@
+console.log("Running 1st File");
+
+function Add(a,b){
+	console.log(a+b);
+}
+
+function Sub(a,b){
+	console.log(a-b);
+}
+
+export {Add, Sub};

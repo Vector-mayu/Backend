13a794e8e8610f8aa4637d7b7e98d70d91363c6a
Vector-mayu <mayureshdandekar143@gmail.com>
2026-01-19 00:41:26 +0530

    Added Routes in Express.js

--- DAY3/express.js
+++ DAY3/express.js
@@ -2,10 +2,22 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res)=>{
-  res.send({"name":"Mayuresh", "age": "19", "gender":"male"});
+app.use("/About", (req, res)=>{
+  res.send("This is Home page Buddy");
 })
 
-app.listen(5000, ()=>{
+app.use("/Profile", (req, res)=>{
+  res.send("This is Profile Page Dude")
+});
+
+app.use("/Category", (req, res)=>{
+  res.send("This is Category Page Silly");
+});
+
+app.use("/",(req, res)=>{
+  res.send({"name":"Mayuresh", "age": "19", "gender":"male"});
+});
+
+app.listen(5000, ()=>{ 
   console.log("App listening at port 5000");
 });

141229f299f84b06104a286468a1dc4f1fbabe1c
Vector-mayu <mayureshdandekar143@gmail.com>
2026-01-21 20:51:20 +0530

    Build Book Store Project using Express

--- DAY4/index.js
+++ DAY4/index.js
@@ -1,16 +1,31 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json()); // used when we do calls like POST... cuz we get data in JSON we need to convert it into JS Object     
+
 const BookStore = [
 	{id:1, name:"Harry Potter", author:"J.K Rowling"},
-	{id:2, name:"Zero to One", author:"Peter Thiel"}
+	{id:2, name:"Zero to One", author:"Peter Thiel"},
+	{id:3, name:"Rich Dad Poor Dad", author:"Robert Kiyosaki"},
+	{id:4, name:"Norweigein Wood", author:"i dont know"}
 ];
 
 app.get("/book", (req, res)=>{
 	res.send(BookStore);
 })
 
+app.get("/book/:id", (req,res)=>{
+	const id = parseInt(req.params.id); // parseInt is used to convert string to INT
+	const ans = BookStore.find(info => info.id === id);
+	console.log(ans);
+	res.send(ans);
+})
+
+app.post("/book", (req,res)=>{
+	BookStore.push(req.body); // data comes in req within {req.body} also can bee seen on postman
+	res.send("Data Saved Successfully in RAM")
+})
 
 app.listen(3000, ()=>{
-	console.log("App Listening on 5000");
+	console.log("App Listening on 3000");
 })

e80a2c7c4628535a5c9fc12c101c5a0b1082d203
Vector-mayu <mayureshdandekar143@gmail.com>
2026-02-14 16:50:38 +0530

    Created CRUD Operations on Database using Mongoose

--- DAY7/index.js
+++ DAY7/index.js
@@ -1,9 +1,48 @@
 const express = require("express");
 const main = require("./Database");
-const User = require("./Models/Users")
+const User = require("./Models/Users");
 const app = express();
 
 app.use(express.json());
+
+// CRUD operations in Database
+
+app.get("/info", async (req, res)=>{
+	const ans = await User.find({city:"Pune"})
+	res.send(ans);
+});
+
+app.post("/info", async (req,res)=>{
+	// const ans = new User(req.body);
+	// await ans.save();
+
+	try{
+		await User.create(req.body);
+	res.send("Successfully Added new User...");
+	}
+	catch(err){
+		res.status(500).send(err);
+	}
+})
+
+app.delete("/info/:name", async (req, res)=>{
+	const name1 = req.params.name;
+	console.log(name1);
+
+	try{
+		const ans = await User.deleteOne({name : name1});
+		res.send(`Deleted ${name1} successfully...`)
+	}
+	catch(err){
+		console.log(err);
+	}
+
+})
+
+app.put("/info", async (req, res)=>{
+	const ans = await User.updateOne({name: "Dhiraj Ahire"}, {city:"Pune"});
+	res.send("Successfully Updated User...")
+})
 
 // Connect DB First then Listen it
 main()
@@ -14,7 +53,7 @@ main()
 		console.log("App Listening at port 3000");
 	})
 
-	const result = await User.find({city:"Pune"});
-	console.log(result);
+	// const result = await User.find({city:"Pune"});
+	// console.log(result);
 })
 .catch((err)=> console.log(err));
